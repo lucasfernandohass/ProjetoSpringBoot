c7d68d817fc46d9cdaa0a3a5bcbafadf7c97223d
Matheus Silva <85744026+Theuxxzz@users.noreply.github.com>
2025-06-15 22:59:50 -0300

    DesenvolvedorService

---
+++ src/main/java/com/esoft/LojaDeJogos/Services/DesenvolvedorService.java
@@ -0,0 +1,53 @@
+package com.esoft.LojaDeJogos.Services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.esoft.LojaDeJogos.DTOs.DesenvolvedorDTO;
+import com.esoft.LojaDeJogos.Expections.NaoEncontradoException;
+import com.esoft.LojaDeJogos.models.Desenvolvedor;
+import com.esoft.LojaDeJogos.repositories.DesenvolvedorRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class DesenvolvedorService {
+
+    public final DesenvolvedorRepository desenvolvedorRepository;
+
+    public DesenvolvedorService(DesenvolvedorRepository desenvolvedorRepository){
+        this.desenvolvedorRepository = desenvolvedorRepository;
+    }
+
+    @Transactional
+    public DesenvolvedorDTO salvar(DesenvolvedorDTO desenvolvedorDTO){
+        Desenvolvedor desenvolvedor = new Desenvolvedor(desenvolvedorDTO);
+        return new DesenvolvedorDTO(desenvolvedorRepository.save(desenvolvedor));
+    }
+
+    @Transactional
+    public DesenvolvedorDTO atualizar(Long id, DesenvolvedorDTO desenvolvedorDTO){
+        Desenvolvedor existente = desenvolvedorRepository.findById(id)
+            .orElseThrow(() -> new NaoEncontradoException("Desenvolvedor com id " + id +"não encotrado."));
+        existente.setNome(desenvolvedorDTO.nome());
+
+        return new DesenvolvedorDTO(desenvolvedorRepository.save(existente));
+    }
+
+    public List<DesenvolvedorDTO> listar(){
+        return desenvolvedorRepository.findAll().stream().map(DesenvolvedorDTO::new).toList();
+    }
+
+    public DesenvolvedorDTO buscarPorId(Long id){
+        return new DesenvolvedorDTO(desenvolvedorRepository.findById(id)
+            .orElseThrow(() -> new NaoEncontradoException("Desenvolvedor com id" +id+ " não encontrado.")));
+    }
+
+    @Transactional
+    public void remover(Long id){
+        Desenvolvedor desenvolvedor = desenvolvedorRepository.findById(id)
+            .orElseThrow(() -> new NaoEncontradoException("Desenvolvedor com id" +id+" não encontrado."));
+        desenvolvedorRepository.deleteById(id);
+    }
+}
