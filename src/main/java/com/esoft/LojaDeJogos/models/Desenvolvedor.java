3f0a6d240fa856a32643ef2d6887631129c6b180
Matheus Silva <85744026+Theuxxzz@users.noreply.github.com>
2025-06-15 22:56:02 -0300

    Classe Desenvolvedor

---
+++ src/main/java/com/esoft/LojaDeJogos/models/Desenvolvedor.java
@@ -0,0 +1,38 @@
+package com.esoft.LojaDeJogos.models;
+
+import com.esoft.LojaDeJogos.DTOs.DesenvolvedorDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_desenvolvedor")
+public class Desenvolvedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    
+    //private List<Jogo> jogos;
+
+    public Desenvolvedor(DesenvolvedorDTO desenvolvedor){
+        this.id = desenvolvedor.id();
+        this.nome = desenvolvedor.nome();
+    }
+}
+
